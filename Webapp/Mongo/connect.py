15c3dd578b3c3cfa93fc8f49739d2510bbc2f07c
Rebecca <rebecca.kane@outlook.com>
2018-03-25 15:05:59 +0100

    Formatting and outputting scores fine from database

--- Webapp/Mongo/connect.py
+++ Webapp/Mongo/connect.py
@@ -7,3 +7,57 @@ app.config['MONGO_DBNAME'] = 'gamescores'
 app.config['MONGO_URI'] = 'mongodb://gestureproj:gest@ds121309.mlab.com:21309/gamescores'
 
 mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    users = mongo.db.users
+    results = users.find()
+
+    output = ''
+    for r in results:
+        old = str(r['score'])
+        score = old[:-2] + ':' + old[-2:]
+        output += r['username'] + '  ' + score + '<br>'
+
+    return output
+ 
+
+@app.route('/add')
+def add():
+    user = mongo.db.users
+    user.insert({ "username": "ay", "score": "410" })
+    user.insert({ "username": "yes", "score": "210" })
+    user.insert({ "username": "john", "score": "401" })
+    user.insert({ "username": "ay", "score": "5332" })
+
+    return 'Added user'
+
+@app.route('/find')
+def find():
+    user = mongo.db.users
+    john = user.find_one({'username': 'john'})
+
+    return 'You found ' + john['username'] + ', score: '  + john['score']
+
+@app.route('/update')
+def update():
+    user = mongo.db.users
+    john = user.find_one({'username': 'john'})
+    newscore = 421
+    oldscore = 401
+    if newscore > oldscore:
+        john['score'] = str(newscore)
+        user.save(john)
+        return "updated score"
+
+    return "not updated"
+
+@app.route('/delete')
+def delete():
+    user = mongo.db.users
+    ay = user.find_one({'username': 'ay'})
+    user.remove(ay)
+    return "removed"
+
+if __name__ == '__main__':
+    app.run(debug=True)
