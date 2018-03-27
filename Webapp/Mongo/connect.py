f4b34172ae544ec5928abd7bc68c931d17a24baf
Rebecca <rebecca.kane@outlook.com>
2018-03-27 12:26:19 +0100

    Outputting sorted list fine, need to work on async

--- Webapp/Mongo/connect.py
+++ Webapp/Mongo/connect.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, json, jsonify, render_template
 from flask_pymongo import PyMongo
+import pymongo
 from bson.json_util import dumps, loads
 
 app = Flask(__name__)
@@ -13,16 +14,16 @@ mongo = PyMongo(app)
 def index():
     user = mongo.db.users
     topscores = user.find() # adapted from https://stackoverflow.com/a/30400268/7232648
-    print(topscores)
-
     return app.send_static_file('index.html')
 
 @app.route('/getScoreList', methods=['GET'])
 def getScoreList():
     user = mongo.db.users
-    topscores = dumps(user.find()) # adapted from https://stackoverflow.com/a/30400268/7232648
+    cursor = user.find()
+    topten = cursor.sort([('score', pymongo.ASCENDING)]).limit(10)
+    topscores = dumps(topten) # adapted from https://stackoverflow.com/a/30400268/7232648
+
     #topscores = loads(topscoresL)
-    print(topscores)
     return topscores
 
 @app.route('/save', methods=['POST'])
@@ -30,6 +31,7 @@ def save():
     print("Reached save backend")
     scoreData = request.json
     print("score data: " + str(scoreData))
+    
     user = mongo.db.users
     user.insert(scoreData)
 
